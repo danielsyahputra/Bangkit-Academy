db149246db8c2e2bfbeb44253861fc62a980f3ff
danielsyahputra13 <dnlshp@gmail.com>
2022-03-02 09:56:37 +0700

    add a puppet file

---
+++ Week3/init.pp
@@ -0,0 +1,10 @@
+class profile {
+        file { '/etc/profile.d/append-path.sh':
+                owner   => 'root',
+                group   => 'root',
+                mode    => '0644',
+                content => "PATH=\$PATH:/java/bin\n",
+        }
+}
+
+# Run: sudo puppet agent -v --test
